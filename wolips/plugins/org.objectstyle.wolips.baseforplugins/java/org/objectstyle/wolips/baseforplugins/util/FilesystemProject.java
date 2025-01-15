c113ec28aeccf7768e46f9b4e165fee9bdc6dc14
Ralf Schuchardt <rasc@gmx.de>
2025-01-15 12:08:51 +0100

    Add @Overrride markers to some methods

--- wolips/plugins/org.objectstyle.wolips.baseforplugins/java/org/objectstyle/wolips/baseforplugins/util/FilesystemProject.java
+++ wolips/plugins/org.objectstyle.wolips.baseforplugins/java/org/objectstyle/wolips/baseforplugins/util/FilesystemProject.java
@@ -138,6 +138,7 @@ public class FilesystemProject extends FilesystemFolder implements IProject {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
 	public String getDefaultLineSeparator() throws CoreException {
 		throw new UnsupportedOperationException();
 	}
